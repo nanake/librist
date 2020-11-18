4fd2b3ea8188a8b5888cf6c4a214457be9442df6
Sergio Ammirata <sergio@ammirata.net>
2020-11-18 13:18:13 -0500

    Add peer type to advanced stats

--- src/stats.c
+++ src/stats.c
@@ -63,6 +63,7 @@ void rist_sender_peer_statistics(struct rist_peer *peer)
 	cJSON_AddNumberToObject(peer_obj, "flow_id", peer->adv_flow_id);
 	cJSON_AddNumberToObject(peer_obj, "id", peer->adv_peer_id);
 	cJSON_AddStringToObject(peer_obj, "cname", peer->receiver_name);
+	cJSON_AddStringToObject(peer_obj, "type", peer->is_data ? "data" : "rtcp");
 	cJSON *json_stats = cJSON_AddObjectToObject(peer_obj, "stats");
 	cJSON_AddNumberToObject(json_stats, "quality", Q);
 	cJSON_AddNumberToObject(json_stats, "sent", (double)peer->stats_sender_instant.sent);
