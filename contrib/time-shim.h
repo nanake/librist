nanake/librist

commit 5b490e9d5687dc31c72b86d11ca8c08642b2f9ca

Don't build clock_gettime shim when it's provided by mingw

contrib/time-shim.h
@@ -8,9 +8,9 @@
 #ifndef __TIME_SHIM_H
 #define __TIME_SHIM_H
 
-# include "config.h"
+#include "config.h"
 
-#if defined(_WIN32)
+#if defined(_WIN32) && !defined (HAVE_CLOCK_GETTIME)
 #define usleep(a)	Sleep((a)/1000)
 # include <winsock2.h>
 # include <time.h>

contrib/time-shim.c
@@ -9,7 +9,7 @@
 
 #include "time-shim.h"
 
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(HAVE_CLOCK_GETTIME)
 #define _WINSOCKAPI_
 # include <windows.h>
 # include <errno.h>

meson.build
@@ -66,6 +66,9 @@ if host_machine.system() == 'windows'
 		add_project_arguments(['-DLIBRIST_BUILDING_DLL'], language: 'c')
 	endif
 	if get_option('have_mingw_pthreads')
+		if cc.has_function('clock_gettime', prefix : '#include <time.h>', args : test_args)
+			cdata.set('HAVE_CLOCK_GETTIME', 1)
+		endif
 		if cc.has_header('pthread.h')
 			threads = cc.find_library('winpthread', static: true)
 			deps += threads
@@ -96,7 +99,7 @@ else
 	elif host_machine.system() != 'darwin'
 		lib_rt = cc.find_library('rt', required: false)
 		if not cc.has_function('clock_gettime', prefix : '#include <time.h>', args : test_args, dependencies : lib_rt)
-		error('clock_gettime not found')
+			error('clock_gettime not found')
 		endif
 		deps += [ lib_rt ]
 		cdata.set('HAVE_CLOCK_GETTIME', 1)

commit d2e62680228df2cd9a5ff5036e3450474687ce4a

Fix regression caused by 5b490e9d

contrib/time-shim.h
@@ -15,10 +15,11 @@
 #include <time.h>
 #define usleep(a)	Sleep((a)/1000)
 
+typedef struct timespec timespec_t;
+
 #ifdef HAVE_CLOCK_GETTIME
 
 #define gettimeofday mingw_gettimeofday
-typedef struct timespec timespec_t;
 
 #else
 