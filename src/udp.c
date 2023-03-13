nanake/librist

commit 2f077a5636f0ef4e03b2c017ee19827b27a4938f

Allow binding on IP (*Nix + win32) or iface name (*nix) via miface param
Only used on outgoing connections.
Fixes #150

src/udp.c
@@ -6,6 +6,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include "logging.h"
 #include "udp-private.h"
 #include "rist-private.h"
 #include "log-private.h"
@@ -499,7 +500,7 @@ void rist_create_socket(struct rist_peer *peer)
 			return;
 		}
 
-		peer->sd = udpsocket_open_bind(host, port, &peer->miface[0]);
+		peer->sd = udpsocket_open_bind(host, port, peer->miface);
 		if (peer->sd >= 0) {
 			rist_log_priv(get_cctx(peer), RIST_LOG_INFO, "Starting in URL listening mode (socket# %d)\n", peer->sd);
 		} else {
@@ -530,6 +531,32 @@ void rist_create_socket(struct rist_peer *peer)
 			rist_log_priv(get_cctx(peer), RIST_LOG_INFO, "Starting in URL connect mode (%d)\n", peer->sd);
 		else {
 			rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Could not start in URL connect mode. %s\n", strerror(errno));
+		}
+		if (peer->miface[0] != '\0') {
+			struct sockaddr_storage ss = {0};
+			rist_log_priv(get_cctx(peer), RIST_LOG_INFO, "Binding socket to %s\n", peer->miface);
+			if (inet_pton(AF_INET, peer->miface,  &((struct sockaddr_in *)&ss)->sin_addr) != 0) {
+				((struct sockaddr_in *)&ss)->sin_family = AF_INET;
+				if (bind(peer->sd, (struct sockaddr*)&ss, sizeof(struct sockaddr_in)) != 0) {
+					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
+				}
+			}
+			else if (inet_pton(AF_INET6, peer->miface, &((struct sockaddr_in6 *)&ss)->sin6_addr) != 0) {
+				((struct sockaddr_in6 *)&ss)->sin6_family = AF_INET6;
+				((struct sockaddr_in6 *)&ss)->sin6_port =0;
+				if (bind(peer->sd, (struct sockaddr*)&ss, sizeof(struct sockaddr_in6)) != 0) {
+					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
+				}
+			}
+#ifndef __WIN32
+			else {
+				struct ifreq ifr = {0};
+				strncpy(ifr.ifr_name, peer->miface, sizeof(ifr.ifr_name));
+				if (setsockopt(peer->sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) != 0) {
+					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
+				}
+			}
+#endif
 		}
 		peer->local_port = 32768 + (get_cctx(peer)->peer_counter % 28232);
 	}

commit fe67a68d165d2ddddda6cbec584365c40dfb6c4a

Fix MacOS build failure introduced in 2f077a56

src/udp.c
@@ -548,13 +548,30 @@ void rist_create_socket(struct rist_peer *peer)
 					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
 				}
 			}
-#ifndef __WIN32
+#ifdef __linux__
 			else {
 				struct ifreq ifr = {0};
 				strncpy(ifr.ifr_name, peer->miface, sizeof(ifr.ifr_name));
 				if (setsockopt(peer->sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) != 0) {
 					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
 				}
+			}
+#elif defined(__APPLE__)
+			else {
+				int idx = if_nametoindex(peer->miface);
+				if (idx == 0) {
+					rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't get device %s index: %s\n", peer->miface, strerror(errno));
+				} else {
+					int proto = peer->u.address.sa_family == AF_INET? IPPROTO_IP : IPPROTO_IPV6;
+					int bound = peer->u.address.sa_family == AF_INET? IP_BOUND_IF : IPV6_BOUND_IF;
+					if (setsockopt(peer->sd, proto, bound, &idx, sizeof(idx)) != 0) {
+						rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "Couldn't bind to %s: %s\n", peer->miface, strerror(errno));
+					}
+				}
+			}
+#else
+			else {
+				rist_log_priv(get_cctx(peer), RIST_LOG_ERROR, "No method available to bind to %s please supply an IP to bind to\n", peer->miface);
 			}
 #endif
 		}