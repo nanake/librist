d7191ea621080fc6449259b43b5bb08c660958f4
Gijs Peskens <gijs@peskens.net>
2023-06-15 10:39:50 +0200

    Add definitions for VSF proto type

--- src/proto/gre.h
+++ src/proto/gre.h
@@ -8,10 +8,17 @@
 #define RIST_GRE_PROTOCOL_TYPE_REDUCED 0x88B6
 #define RIST_GRE_PROTOCOL_TYPE_FULL 0x0800
 #define RIST_GRE_PROTOCOL_TYPE_EAPOL 0x888E
+#define RIST_GRE_PROTOCOL_TYPE_VSF 0xCCE0
 #define RIST_GRE_PROTOCOL_REDUCED_SIZE 4
 
 #define RIST_GRE_FLAGS_KEY_SEQ 0x000C
 #define RIST_GRE_FLAGS_SEQ     0x0008
+
+#define RIST_VSF_PROTOCOL_TYPE_RIST 0x0000
+
+#define RIST_VSF_PROTOCOL_SUBTYPE_REDUCED 0x0000
+#define RIST_VSF_PROTOCOL_SUBTYPE_KEEPALIVE 0x8000
+#define RIST_VSF_PROTOCOL_SUBTYPE_FUTURE_NONCE 0x8001
 
 /*
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -45,6 +52,17 @@ Reduce overhead GRE payload header (only one supported for now)
 
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 |        source port            |      destination port         |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Keep alive message
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|0| |1|1| Reserved0       | Ver |     Protocol Type = 0x88B5    |
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|																|
+| 48-bit MAC Address            +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|   					        |X|R|B|A|P|E|L|N|D|T|V|J|F|Rsvd1|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+| 			  Message Payload (JSON format)						|
 +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 
@@ -102,6 +120,11 @@ RIST_PACKED_STRUCT(rist_gre_key_seq,{
 	uint32_t checksum_reserved1;
 	uint32_t nonce;
 	uint32_t seq;
+})
+
+RIST_PACKED_STRUCT(rist_vsf_proto,{
+	uint16_t type;
+	uint16_t subtype;
 })
 
 #endif /* RIST_PROTO_GRE_H */
